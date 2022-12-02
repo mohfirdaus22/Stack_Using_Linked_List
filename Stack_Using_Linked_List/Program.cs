4d9fd0cd3f3b98bf53f13bc5f4cdd36f9269586c
mohfirdaus22 <thepashter6@gmail.com>
2022-12-02 17:11:42 +0700

    Add case 1

--- Stack_Using_Linked_List/Program.cs
+++ Stack_Using_Linked_List/Program.cs
@@ -80,7 +80,11 @@ namespace StackUsingLinkedList_CSharp
                     char ch = Convert.ToChar(sInput == "" ? "0" : sInput);
                     switch (ch)
                     {
-
+                        case '1':
+                            Console.Write("\nEnter a number: ");
+                            int num = Convert.ToInt32(Console.ReadLine());
+                            s.push(num);
+                            break;
                     }
                 }
             }
